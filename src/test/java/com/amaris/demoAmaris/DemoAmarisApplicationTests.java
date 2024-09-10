f7828faadf2a458a603e5e044c8b36324b247544
m4st3 <runtycybin@gmail.com>
2024-09-10 10:16:58 +0200

    Test class

---
+++ src/test/java/com/amaris/demoAmaris/DemoAmarisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amaris.demoAmaris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAmarisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
